4ebb49d34709632573b7b8f04efe333cf6f788e3
APratham <a.pratham1998@gmail.com>
2024-08-10 13:53:39 +0100

    Added profile pic feature

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -1,13 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { UserInfoService } from '../user-info.service';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './topbar.component.html',
   styleUrls: ['./topbar.component.scss']
 })
-export class TopBarComponent {
-  // You can add @Input properties later if you want to make the text/logo dynamic
+export class TopBarComponent implements OnInit {
   @Input() title: string = 'Default Title';
   @Input() logo: string = '/assets/logo.svg'; // Path to a dummy SVG for now
-  @Input() profilePic: string = 'assets/profile/avatar.jpg'; // Path to a dummy avatar for now
+  profilePic: string = 'assets/profile/avatar.jpg'; // Default avatar
+
+  constructor(private userInfoService: UserInfoService) {}
+
+  ngOnInit() {
+    const userInfo = this.userInfoService.getUserInfo();
+    if (userInfo && userInfo.picture) {
+      this.profilePic = userInfo.picture; // Update with the actual profile picture URL
+    }
+
+    // Listen to changes in userInfo (in case the data arrives after the component is initialized)
+    if (window.electron) {
+      window.electron.ipcRenderer.on('user-info', (event, userInfo) => {
+        if (userInfo && userInfo.picture) {
+          this.profilePic = userInfo.picture;
+        }
+      });
+    }
+  }
 }
