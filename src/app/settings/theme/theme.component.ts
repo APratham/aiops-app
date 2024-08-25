f0763201302e9f3815a8b6d8143aa5cd9bde6e2b
APratham <a.pratham1998@gmail.com>
2024-08-25 10:35:06 +0100

    Added settings items

--- src/app/settings/theme/theme.component.ts
+++ src/app/settings/theme/theme.component.ts
@@ -3,12 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-theme',
   templateUrl: './theme.component.html',
-  styleUrls: ['./theme.component.scss'],
+  styleUrls: ['./theme.component.scss']
 })
-export class ThemeComponent  implements OnInit {
+export class ThemeComponent implements OnInit {
+
+  settings = {
+    theme: 'light'
+  };
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    // Load settings if needed
+  }
 
+  saveSettings() {
+    // Save settings to storage or database
+  }
 }
