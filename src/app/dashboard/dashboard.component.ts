b6ee3ff11a29323b2f3b8118cd897523e08734f4
APratham <a.pratham1998@gmail.com>
2024-09-03 09:49:26 +0100

    Added Drag and Drop

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,43 +1,60 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { IpcRendererEvent } from 'electron';
- 
-interface ApiResponse {
-  message: string;
+import { CdkDragDrop, CdkDragEnd, transferArrayItem } from '@angular/cdk/drag-drop';
+
+interface ContainerItem {
+  content: string;
+  transform?: string;  // To store the drag position
 }
+
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-
 export class DashboardComponent implements OnInit {
-
-
   containerData: any;
+  sideContainerItems: ContainerItem[] = [
+    { content: 'Item 1' },
+    { content: 'Item 2' },
+  ];
 
-  constructor(private http: HttpClient, private router: Router) { }
+  pageContainerItems: ContainerItem[] = [
+    { content: 'Page Item 1' },
+    { content: 'Page Item 2' },
+  ];
 
-  ngOnInit(): void {
+  sideContainerVisible = true;
 
+  toggleSideContainer(): void {
+    this.sideContainerVisible = !this.sideContainerVisible;
+  }
 
-    window.electron.ipcRenderer.on('container-data', (event, data) => {
-      this.containerData = data;
-      console.log('Received container data:', this.containerData);
-    });
+  onDrop(event: CdkDragDrop<ContainerItem[]>) {
+    if (event.previousContainer === event.container) {
+      return;
+    }
 
-  
+    if (event.previousContainer.id === 'sideContainer' && event.container.id === 'pageContainer') {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 
-  
-
- 
-
-
-
+  onDragEnd(event: CdkDragEnd, item: ContainerItem) {
+    const transform = `translate3d(${event.source.getFreeDragPosition().x}px, ${event.source.getFreeDragPosition().y}px, 0)`;
+    item.transform = transform;  // Save the position to maintain it after drop
+    event.source.element.nativeElement.style.transform = transform;
   }
 
 
-  
+  ngOnInit(): void {
+    window.electron.ipcRenderer.on('container-data', (event, data) => {
+      console.log('Received container data:', data);
+    });
+  }
 }
