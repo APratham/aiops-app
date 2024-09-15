1aba9653095e6f4b669d08f90c2a76a3aa2e129d
APratham <a.pratham1998@gmail.com>
2024-09-15 07:05:15 +0100

    Added logs component

--- src/app/logs/logs.component.ts
+++ src/app/logs/logs.component.ts
@@ -6,9 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./logs.component.scss'],
 })
 export class LogsComponent  implements OnInit {
+  activeSegment: string = 'all';
+  logs: { message: string; resultCount: string; }[] = [];
+  inputFilter: string | RegExp = '';
+  filteredLogs!: { message: string; resultCount: string; }[];
+
+  onSegmentChange(segment: any) {
+    this.activeSegment = segment.detail.value;
+  }
+
+  onFilterLogs() {
+    try {
+      const regex = new RegExp(this.inputFilter, 'i');  // Create regex from user input, case-insensitive
+      this.filteredLogs = this.logs.filter(log => regex.test(log.message));
+    } catch (e) {
+      console.error('Invalid regex pattern:', e);
+      this.filteredLogs = [...this.logs];  // If regex is invalid, show all logs
+    }
+  }
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.logs = [
+      { message: 'Log message', resultCount: '10,000' },
+      { message: 'Log message', resultCount: '10,000' },
+      { message: 'Log message', resultCount: '10,000' },
+      { message: 'Log message', resultCount: '10,000' },
+      { message: 'Log message', resultCount: '10,000' },
+      { message: 'Log message', resultCount: '10,000' },
+    ];
+    this.filteredLogs = [...this.logs];
+  }
 
 }
